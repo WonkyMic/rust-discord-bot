0cc9fcf56cb6e814d8fb969a0e3fb8c02bcca1bc
wonkymic <wonkymic@gmail.com>
2021-11-20 19:15:52 -0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# rust-discord-bot
+Playing around with Event Listeners in Rust.
+
+## Resources
+- [serenity. A library for the Discord API](https://crates.io/crates/serenity)
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+extern crate dotenv;
+use dotenv::dotenv;
+use std::env;
+
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    // Messages
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+    // Readiness
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let mut client = 
+        Client::builder(&token).event_handler(Handler).await.expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
